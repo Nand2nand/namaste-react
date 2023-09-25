1858f1af12e9f9883e025412b6fdc2696dfe28cb
Nand2nand <nanad280494@gmail.com>
2023-09-25 19:12:03 +0530

    update code

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import  ReactDOM from 'react-dom/client';
 const parent  = React.createElement('h1',{},"hello");
+//React element
+const title =   (
+<h1 className="heading">
+    Namaste React using JSX
+    </h1>
+    );
+// react functional component
+const HeadingComponent = () => (
+<div id="container">
+   {title}
+<h1>Namaste react functional component</h1>
+</div>
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent)
+root.render(<HeadingComponent/>)
